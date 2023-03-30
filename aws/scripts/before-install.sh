ba5304896d55b1694732882accc9785efa377b06
Juan Jaime <jjaime@domainlogic.io>
2023-03-30 11:52:33 +0200

    crear directorio de despliegue si no existe

--- aws/scripts/before-install.sh
+++ aws/scripts/before-install.sh
@@ -6,4 +6,10 @@ if [ -d /usr/local/codedeployresources ]; then
     rm -rf /usr/local/codedeployresources/
 fi
 
+if [ -d /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war ]; then
+    rm -rf /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+fi
+
 mkdir -vp /usr/local/codedeployresources
+mkdir -vp /usr/local/tomcat9/webapps
+
